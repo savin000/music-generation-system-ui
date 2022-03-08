637772c7046706d16c3df7434390b8a43300a5d3
savin000 <58307835+savin000@users.noreply.github.com>
2022-03-08 22:05:01 +0300

    refactor packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
-import {LoginComponent} from "./login/login/login.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { LoginFormComponent } from './login/login/login-form/login-form.component';
-import { RegistrationFormComponent } from './login/login/registration-form/registration-form.component';
+import { LoginComponent } from './login/login.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { RegistrationFormComponent } from './login/registration-form/registration-form.component';
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
+import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './home/player/player.component';
+import { CalendarComponent } from './home/calendar/calendar.component';
+import { AboutComponent } from './home/about/about.component';
+import { MusicPlayerComponent } from './home/music-player/music-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     LoginFormComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    HomeComponent,
+    PlayerComponent,
+    CalendarComponent,
+    AboutComponent,
+    MusicPlayerComponent
   ],
   imports: [
     BrowserModule,
