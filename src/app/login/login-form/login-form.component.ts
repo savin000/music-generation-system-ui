4a4954e74d11c20ff2c8449ea14c37e71ca22dc6
savin000 <58307835+savin000@users.noreply.github.com>
2022-03-13 00:30:12 +0300

    add navigation (without actually logging)

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-form',
@@ -8,17 +9,16 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 export class LoginFormComponent implements OnInit {
   @Output() signUpButtonClick: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   logInButtonClicked() {
-
+    this.router.navigate(['home']);
   }
 
   signUpButtonClicked() {
     this.signUpButtonClick.emit(true)
   }
-
 }
